fab87bc895e37b40443276bfdbf962e0ce374fae
unknown <ermalmiftari676@gmail.com>
2025-07-05 17:31:51 +0200

    .

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,13 @@
-// src/layouts/MainLayout.tsx
+// Updated src/layouts/MainLayout.tsx
 import { ReactNode } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-stone-950 text-white font-sans">
       <Navbar />
-      
-      <main className="flex-grow  bg-gray-50">{children}</main>
+      <main className="flex-grow w-full px-4 sm:px-6 md:px-8 py-6">{children}</main>
       <Footer />
     </div>
   );
